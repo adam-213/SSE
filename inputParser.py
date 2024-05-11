c2414dcbea6e607df2e82fc99181945da0b83a55
Adam <git@chradam.xyz>
2024-05-11 22:02:45 +0200

    Add Dockerfile, Readme Change docstrings to be more informative Change to snake_case

--- inputParser.py
+++ inputParser.py
@@ -1,106 +1,96 @@
 import argparse as ap
-import os.path
-import pandas as pd
 import logging
+import os.path
+
+import pandas as pd
 
 
 # Assumptions
-# SSV file format
-# It is quite difficult to have 2 positional arguments while having the input be extensible whle the other are not -> -parameters were used
-# there might be concerns with memory with this kind of loading,
-#   there are options, you could use vaex [out of core] or process the files as they are being loaded 1by1
-
+# SSV file format = .log
+# It is quite difficult to have 2 positional arguments while when one (or more) of them are extensible
 
 class InputParse:
     def __init__(self):
-        self.parser = ap.ArgumentParser(prog='LogParse', description='Parse SquidProxy logs, currently handles CSV files')
+        self.parser = ap.ArgumentParser(prog='LogParse',
+                                        description='Parse SquidProxy logs, currently handles CSV files')
         # flag args format (flag, help) : True / False
-        self.flagArgs = [('--mfip', 'Most frequent IP'),
-                         ('--lfip', 'Least frequent IP'),
-                         ('--eps', 'Events per Second'),
-                         ('--bytes', 'Total amount of bytes exchanged')]
+        self.flag_args = [('--mfip', 'Most frequent IP'),
+                          ('--lfip', 'Least frequent IP'),
+                          ('--eps', 'Events per Second'),
+                          ('--bytes', 'Total amount of bytes exchanged')]
         self.files = []
 
-        self.addArgs()
+        self.add_args()
 
-    def addArgs(self):
-        # Required files arg (extensible)
+    def add_args(self):
+        """Add required arguments to the argparse"""
         self.parser.add_argument('-i', "--inputFilePath", nargs="+",
                                  help="One or more files to parse", action="store")
-        self.parser.add_argument('-o', '--outputFilePath', nargs=1,
+        self.parser.add_argument('-o', '--outputFilePath', type=str,
                                  help=" Path to a file to save output in plain text JSON format")
 
         # Add Required flags (at least one of)
-        requiredFlag = self.parser.add_argument_group("Output Information")
-        for arg in self.flagArgs:
-            requiredFlag.add_argument(arg[0], help=arg[1], action="store_true", required=False)
+        required_flag = self.parser.add_argument_group("Output Information")
+        for arg in self.flag_args:
+            required_flag.add_argument(arg[0], help=arg[1], action="store_true", required=False)
 
-    def verifyFileLocations(self, args):
-        notFound = []
-        notReadable = []
+    def verify_file_locations(self, args) -> None:
+        """Verify permissions for input and output files before attempting any file operations.
+         There might be different options of handling this situation, for now it just raises a permissions error"""
+        not_found = []
+        not_readable = []
         for file in args.inputFilePath:
             if not os.path.exists(file):
-                notFound.append(file)
+                not_found.append(file)
             if not os.access(file, os.R_OK):
-                notReadable.append(file)
-        if notFound:
-            raise FileNotFoundError("\n".join(notFound))
-        if notReadable:
-            raise PermissionError('Unable to read [Permission Denied]: \n' + "\n".join(notFound))
+                not_readable.append(file)
+        if not_found:
+            raise FileNotFoundError("\n".join(not_found))
+        if not_readable:
+            raise PermissionError('Unable to read [Permission Denied]: \n' + "\n".join(not_readable))
         if not os.access(args.outputFilePath[0], os.W_OK):
             raise PermissionError(f"Write permissions denied for {args.outputFilePath}")
 
-    def loadFiles(self, files: list):
-        for filePath in files:
-            logging.info(f"Loading file: {filePath}")
-            if filePath.endswith(".log"):
+    def load_files(self, files: list):
+        """Load all files into memory in form of pandas dataframes for further processing.
+        Very likely quite memory heavy, there are optimization paths like out of core or 1by1 / parallel processing"""
+        for file_path in files:
+            logging.info(f"Loading file: {file_path}")
+            if file_path.endswith(".log"):
                 try:
                     # Assuming that .log means SSV as is usual
-                    fileContent = self.parseSSV(filePath)
-                    # appending dataframes to a list for them to be returned by the class.
-                    self.files.append((filePath, fileContent))
+                    file_content = self.parse_ssv(file_path)
+                    self.files.append((file_path, file_content))
                 except Exception as e:
-                    logging.error(f"File not in expected format[SSV] skipping: {filePath} \n {e}")
+                    logging.error(f"File not in expected format[SSV] skipping: {file_path} \n {e}")
             # Add more elif conditions as desired to expand file reading ability
             # (Could be made into standalone file if there is enough types as a class extension)
             else:
-                logging.error(f"File format not recognised: {filePath}")
+                logging.error(f"File format not recognised: {file_path}")
 
         logging.info("Files loaded")
 
-    def parseSSV(self, file):
+    def parse_ssv(self, file: str) -> pd.DataFrame:
+        """Uses pandas read_csv function with tweaks and python engine to load SSV files,
+         logs any bad lines as warnings"""
 
-        def handleBadLine(line):
+        def handle_bad_line(line):
             # handling if required
             logging.warning(f"Bad Line {line}")
 
-        data = pd.read_csv(file, sep='\\s+', on_bad_lines=handleBadLine, engine='python')
+        data = pd.read_csv(file, sep='\\s+', on_bad_lines=handle_bad_line, engine='python')
         # cleaning here if required
         return data
 
     def run(self):
         args = self.parser.parse_args()
-        # create a dict of flagname : flagvalue
-        flagValues = {flag[0].strip('-'): getattr(args, flag[0].strip('-')) for flag in self.flagArgs}
+        # create a dict of flag name : flag value
+        flag_values = {flag[0].strip('-'): getattr(args, flag[0].strip('-')) for flag in self.flag_args}
         # check if at least 1 flag is set
-        if sum(flagValues.values()) == 0:
+        if sum(flag_values.values()) == 0:
             self.parser.error("At least of the parameters in the Output Information flags is needed")
 
-        self.verifyFileLocations(args)
-        self.loadFiles(args.inputFilePath)
+        self.verify_file_locations(args)
+        self.load_files(args.inputFilePath)
 
-        if __name__ == '__main__':
-            # for self testing purposes
-            self.test(args, flagValues)
-
-        return self.files, flagValues, args.outputFilePath[0]
-
-    def test(self, args, flagValues):
-        logging.debug(args)
-        logging.debug(self.files)
-        logging.debug(flagValues)
-
-
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.DEBUG)
-    InputParse().run()
+        return self.files, flag_values, args.outputFilePath
